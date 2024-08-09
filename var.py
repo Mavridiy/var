9d57d2ea0810274e82e59fd79d35e77817e02e95
Mavridiy <161286685+Mavridiy@users.noreply.github.com>
2024-08-09 23:17:11 +0300

    Add files via upload

---
+++ var.py
@@ -0,0 +1,5 @@
+hw_count = 12
+all_time = 1.5
+course = 'Python'
+time_to_task = hw_count // all_time
+print('Курс:',course,', всего задач:', hw_count, ' затрачено часов:', all_time, ', среднее время выполнения:', time_to_task)
